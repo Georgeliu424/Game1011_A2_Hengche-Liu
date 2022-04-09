b88a5e0644d271fe53a916770bba74e6ef084b33
Georgeliu <liuhengche424@gmail.com>
2022-04-09 19:58:56 -0400

    添加项目文件。

---
+++ Game1011_Assignment 2_Hengche Liu/intro.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <fstream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	
+	fstream file;
+	string input;
+
+	file.open("Intro.txt", ios::in);
+	if (file.fail())
+	{
+		cout << "File open error !" << endl;
+		return 1;
+	}
+
+	getline(file, input);
+
+	while (!file.fail())
+	{
+		cout << input<<endl;
+		getline(file, input);
+
+	}
+	
+	file.close();
+
+	return 0;
+}
